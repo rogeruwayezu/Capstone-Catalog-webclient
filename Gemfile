150d19d07f098fad7f22a51f83d18792a16de75c
sandrinenikuze <nikuzesandrine@yahoo.fr>
2017-03-10 11:08:04 +0200

    Add controller and bootstrap to the app

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'bootstrap-sass', '~> 3.3.6'
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.0.5'
--- app/controllers/capstones_controller.rb
+++ app/controllers/capstones_controller.rb
@@ -1,2 +1,13 @@
 class CapstonesController < ApplicationController
+
+  def index
+    @students = Unirest.get("http://localhost:3000/studdents.json").body
+    @capstones = Unirest.get("http://localhost:3000/capstones.json").body
+  end
+  
+  def show
+    @student = Unirest.get("http://localhost:3000/studdents/#{params[:id]}.json").body
+    @capstone = Unirest.get("http://localhost:3000/capstones/#{params[:id]}.json").body
+  end
+  
 end
