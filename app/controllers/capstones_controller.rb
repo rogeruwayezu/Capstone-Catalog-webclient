1c4c9b28d30be6ceebfcda21d9e57b729ca9ba10
Jean Luc Abayo <luc.bayo@gmail.com>
2017-03-14 15:28:23 +0200

    updated capstone controller

--- app/controllers/capstones_controller.rb
+++ app/controllers/capstones_controller.rb
@@ -1,9 +1,10 @@
 class CapstonesController < ApplicationController
   def index
-    @capstones = Unirest.get("http://localhost:3000/capstones.json", headers:{"Accept"=>"application/json"}).body
-  end
+     @capstones = Unirest.get("http://localhost:3000/capstones.json", headers:{"Accept"=>"application/json"}).body
+   end
 
   def show
-    @capstone = Unirest.get("http://localhost:3000/capstones/#{params[:id]}.json", headers:{"Accept"=>"application/json"}).body
+    @capstone = Unirest.get("http://localhost:3000/capstones/#{params['id'].to_i}.json", headers:{"Accept"=>"application/json"}).body
+    render :show
   end
 end
