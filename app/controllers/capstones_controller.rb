43b0c6471872a75ef9ac3cd99c4ca936f8310867
sandrinenikuze <nikuzesandrine@yahoo.fr>
2017-03-14 15:42:42 +0200

    updated index

--- app/controllers/capstones_controller.rb
+++ app/controllers/capstones_controller.rb
@@ -1,7 +1,7 @@
 class CapstonesController < ApplicationController
 
   def index
-    @capstones = Unirest.get("http://localhost:3000/capstones.json", headers:{"Accept"=>"application/json", "Authorization"=>"Token token=#{}", "X-User-Email"=>""}).body
+    @capstones = Unirest.get("http://localhost:3000/capstones.json", headers:{"Accept"=>"application/json"}).body
   end
 
   def show
