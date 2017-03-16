0083e869c2ea4032c44a0167e7a92250b9ee00ce
sandrinenikuze <nikuzesandrine@yahoo.fr>
2017-03-16 12:02:24 +0200

    changes on the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "capstones#index"
   get "/capstones", to: 'capstones#index'
   get "/capstones/:id", to:'capstones#show'
 
